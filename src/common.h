3156ce3ceec851d0bce582af5b72f28c52b8e7e6
Ciro S. Costa <ciro.costa@liferay.com>
2017-06-24 12:28:06 -0300

    Standardizes coding

--- src/common.h
+++ src/common.h
@@ -1,11 +1,16 @@
 #ifndef MEM__COMMON_H
 #define MEM__COMMON_H
 
+#include <ctype.h>
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+#define KB(x) ((size_t)(x) << 10)
+#define MB(x) ((size_t)(x) << 20)
 #define TRUE 1
 #define FALSE 0
 
@@ -34,6 +39,55 @@ is_num(char str[])
 	}
 
 	return TRUE;
+}
+
+typedef struct args_t {
+	int n;
+} args_t;
+
+void
+parse_args(int argc, char** argv, args_t* args)
+{
+	char* nvalue = NULL;
+	int pids = 0;
+	int c;
+
+	_must(argc > 1, "%s\n", "Argument -n must be specified.");
+
+	opterr = 0;
+	while ((c = getopt(argc, argv, "n:")) != -1) {
+		switch (c) {
+			case 'n':
+				nvalue = optarg;
+				_must(is_num(nvalue), "Argument (n) must be an "
+				                      "int (# of pids to "
+				                      "create)");
+				break;
+		}
+	}
+
+	_must(nvalue, "'n' arguments (number of pids') must be specified.");
+	_must(nvalue > 0, "At least 1MB must be written");
+
+	args->n = atoi(nvalue);
+}
+
+void
+handle_sigint(int signum)
+{
+	printf("SIGINT received.\n");
+}
+
+void
+wait_until_signalized()
+{
+	struct sigaction action;
+	memset(&action, 0, sizeof(struct sigaction));
+	action.sa_handler = handle_sigint;
+	sigaction(SIGINT, &action, NULL);
+
+	printf("[%d] Waiting for SIGINT\n", getpid());
+	sleep(36000);
 }
 
 #endif // ! MEM_COMMON_H
--- src/disk.c
+++ src/disk.c
@@ -1,13 +1,5 @@
-#include <ctype.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include "./common.h"
 
-#define KB(x) ((size_t)(x) << 10)
-#define MB(x) ((size_t)(x) << 20)
 #define CHUNK_SIZE KB(4)
 
 const char* USAGE = "\n\
@@ -18,41 +10,6 @@ const char* USAGE = "\n\
     disk -n <number of megabytes>\n\
 \n\
 ";
-
-typedef struct args_t {
-	int n;
-} args_t;
-
-void
-parse_args(int argc, char** argv, args_t* args)
-{
-	char* nvalue = NULL;
-	int pids = 0;
-	int c;
-
-	_must(argc > 1, "%s\n  %s", "Argument -n must be specified.", USAGE);
-
-	opterr = 0;
-	while ((c = getopt(argc, argv, "n:")) != -1) {
-		switch (c) {
-			case 'n':
-				nvalue = optarg;
-				_must(is_num(nvalue), "Argument (n) must be an "
-				                      "int (# of pids to "
-				                      "create)");
-				break;
-			case '?':
-			default:
-				printf("%s", USAGE);
-				abort();
-		}
-	}
-
-	_must(nvalue, "'n' arguments (number of pids') must be specified.");
-	_must(nvalue > 0, "At least 1MB must be written");
-
-	args->n = atoi(nvalue);
-}
 
 void
 write_to_disk(int count)
@@ -75,6 +32,7 @@ main(int argc, char** argv)
 
 	parse_args(argc, argv, &args);
 	write_to_disk(args.n);
+	wait_until_signalized();
 
 	return 0;
 }
